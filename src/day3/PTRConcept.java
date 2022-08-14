ab12e93bbab759770dc3c2d5bcacd734702355da
AJAYC2048 <104680021+AJAYC2048@users.noreply.github.com>
2022-08-14 00:16:31 -0500

    PTR Concept

---
+++ src/day3/PTRConcept.java
@@ -0,0 +1,31 @@
+package day3;
+
+public class PTRConcept {
+
+    public static void main(String[] args) {
+
+        int principal = 10000;
+        int time = 1;
+        int rate = 5;
+
+        int interestRate = (principal * time * rate / 100);
+        System.out.println(interestRate);
+
+    }
+
+    public static void main(String[] args) {
+
+
+        int p = 2000;
+        double t = 0.42;
+        int r = 3;
+
+        double interestRate1 = (p * t * r) / 100;
+        System.out.println(interestRate1);
+
+
+    }
+
+
+
+}
