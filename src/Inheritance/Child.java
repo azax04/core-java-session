4084871b21c6e3ea3f75d98030043fd346967e0d
AJAYC2048 <104680021+AJAYC2048@users.noreply.github.com>
2022-08-15 23:10:35 -0500

    Inheritance concept

--- src/Inheritance/Child.java
+++ src/Inheritance/Child.java
@@ -5,6 +5,10 @@ public class Child {
         Child child1 = new Child();
         child1.setLastName("AK");
         child1.setAge(30);
+        child1.setHeight(5.9);
+        child1.setGenetics("Biological");
+
+
 
 
     }
